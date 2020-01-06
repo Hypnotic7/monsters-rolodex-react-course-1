a90006016583fe46901416bfce7e16106836c4fd
Mike <wrzosekm71@gmail.com>
2020-01-06 18:35:23 +0000

    Updates

---
+++ precache-manifest.ec68136cec5fc9a1e9f36c34819b6cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2665ea15b1cfebd0ef9c839723dd24",
+    "url": "/monsters-rolodex-react-course-1/index.html"
+  },
+  {
+    "revision": "aa741b244ced642e8379",
+    "url": "/monsters-rolodex-react-course-1/static/css/main.9fe6145a.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex-react-course-1/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex-react-course-1/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa741b244ced642e8379",
+    "url": "/monsters-rolodex-react-course-1/static/js/main.99bbb096.chunk.js"
+  },
+  {
+    "revision": "72c5796b9885a5b9a372",
+    "url": "/monsters-rolodex-react-course-1/static/js/runtime-main.a91bd0a2.js"
+  }
+]);
